6056d8471cc306672188453a3dd811b219addb69
Ashwathnair237 <ashwathnair237@gmail.com>
2026-06-16 10:41:15 +0530

    Functions: add and vote functions

---
+++ sum2nos.js
@@ -0,0 +1,10 @@
+console.log(`Adding two nos`);
+
+function add (first,second){
+
+    return first + second;
+
+}
+no = add(2,5);
+console.log(no);
+
---
+++ votingage.js
@@ -0,0 +1,9 @@
+console.log(`voting criteria guys`);
+function vote(name,age){
+
+    return age >= 18;
+}
+let x = vote('crazy',7);
+let y = vote('loverm',20);
+console.log(x);
+console.log(y);
