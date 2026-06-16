080858836c9e746dfe405e364f7ea428888f82b1
Ashwathnair237 <ashwathnair237@gmail.com>
2026-06-16 10:15:29 +0530

    Functions: girlname function with template literals

---
+++ functions.js
@@ -0,0 +1,6 @@
+console.log("this is how functions i guess!!")
+function girlname (name,power){
+console.log(name + power , "=== strength");
+}
+girlname('pink ranger', ' super cute');
+girlname('yellow ranger', ' invisiblity');
