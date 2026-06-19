466cc99173de7aa7bb641495941ad66fd0c5bfa0
Ashwathnair237 <ashwathnair237@gmail.com>
2026-06-19 18:26:52 +0530

    Loops and arrays - all exercises

--- iteration.js
+++ iteration.js
@@ -2,13 +2,17 @@ function upperCase (str){
     return str.toUpperCase();
 }
 
-let birds =['pidegeon', 'spaarow', 'swan', 'kingfisher', 'owl'];
+function kstr(k){
+    return k.startsWith('k');
+}
+
+let birds =['pidegeon', 'sparow', 'swan', 'kingfisher', 'owl','kite','kingbird'];
 
 for (let bird of birds){
     console.log(bird)
 }
 
 let upperBirds = birds.map(upperCase);
-
+let kletter = birds.filter(kstr);
 console.log(upperBirds);
-
+console.log(kletter);
