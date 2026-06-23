416b61d893c38059be21cc8fcd841987f119ea42
Ashwath <ashwathnair237@gmail.com>
2026-06-23 16:35:29 +0530

    Add basic calculator functions: add, subtract, multiply, divide

---
+++ calculatorrough.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+console.log(add(2,5))
+console.log(subtract(10,7))
+console.log(add(2,5))
+console.log(divide(12,6))
