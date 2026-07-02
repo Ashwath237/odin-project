b52491617f0adcade03848a90e312ef995312ce8
Ashwath <ashwathnair237@gmail.com>
2026-07-02 11:00:48 +0530

    Add file serving with fs module

--- basic-informational-site/Ronaldo.js
+++ basic-informational-site/Ronaldo.js
@@ -1,11 +1,12 @@
 const http = require("http");
 const fs = require('fs');
+require('dotenv').config();
 
 const server = http.createServer((req,res) =>{
     res.writeHead(200, { 'Content-Type': 'text/html' });
     if (req.url ==="/hello"){
         const helloHtml = fs.readFileSync('./hello.html', 'utf-8');
-        res.end(indexHTML);
+        res.end(helloHtml);
     }
     else if (req.url === "/hey") {
         const heyHtml = fs.readFileSync('./hey.html', 'utf-8');
@@ -23,6 +24,6 @@ const server = http.createServer((req,res) =>{
 
 
 });
-
+console.log(process.env.API_KEY);
 server.listen(3000);
 console.log('Server running on http://localhost:3000');
