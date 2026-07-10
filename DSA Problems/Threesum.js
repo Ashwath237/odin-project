c509611888c06766147354d0a79b30caf202c2f0
Ashwath <ashwathnair237@gmail.com>
2026-07-10 08:26:28 +0530

    Three Sum with duplicate handling - brute force O(n³)

--- DSA Problems/Threesum.js
+++ DSA Problems/Threesum.js
@@ -2,10 +2,16 @@ const nums = [-1, 0, 1, 2, -1, -2];
 const target = 0;
 
 
-var twoSum = function(nums, target){
+var threeSum = function(nums, target){
     for(let i = 0 ; i < nums.length ;i++ ){
+        if (i > 0 && nums[i] === nums[i-1]) 
+            continue;
         for (let j = i+1 ; j< nums.length ; j++){
+            if (j > 0 && nums[j] === nums[j-1]) 
+                continue;
             for (let k = j+1 ; k <nums.length ; k++){
+                if (k > 0 && nums[k] === nums[k-1]) 
+                    continue;
                 if (nums[i] + nums[j] + nums[k] === target){
                     return[i,j,k];
                 }
@@ -16,3 +22,5 @@ var twoSum = function(nums, target){
 
     } 
 }
+
+console.log(threeSum);
